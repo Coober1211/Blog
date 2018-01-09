77238ac4c1125d0cf7398a7a8354ecfd2e85647d
mercer <skydreamfly@gmail.com>
2018-01-09 23:40:21 +0800

    add login form

---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login-form">
+    <form action="POST">
+      <input type="email" name="email" id="email">
+      <br>
+      <input type="password" name="password" id="password">
+      <br>
+      <button @click="login">Login!</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    login() {
+      // eslint-disable-next-line
+      console.log('Hello');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.login-form {
+  display: inline-block;
+  margin-top: 100px;
+  margin-left: 50%;
+  transform: translateX(-50%);
+
+}
+</style>
+
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Articles from '@/components/Articles';
+import LoginForm from '@/components/LoginForm';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/articles',
       name: 'Articles',
       component: Articles,
+    },
+    {
+      path: '/admin',
+      name: 'LoginForm',
+      component: LoginForm,
     },
   ],
   mode: 'history',
