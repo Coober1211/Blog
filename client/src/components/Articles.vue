1f6961cc8707c23c5d7ebab0c23b66f0b9c290f9
mercer <skydreamfly@gmail.com>
2018-01-09 14:49:46 +0800

    add articles component in client

---
+++ client/src/components/Articles.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .article
+    .header
+    .main
+</template>
+
+<script>
+export default {
+  async created() {
+    try {
+      const response = await this.$http.get('http://localhost:3000/article');
+      const articles = response.data.articles;
+      // eslint-disable-next-line
+      console.log(articles);
+    } catch (err) {
+      // eslint-disable-next-line
+      console.log(err);
+    }
+  },
+};
+</script>
+
