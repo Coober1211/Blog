0730dcebbd501d7659cba5f4c227c2825e7014e7
mercer <skydreamfly@gmail.com>
2018-01-22 16:26:43 +0800

    modify axios baseurl

--- client/src/services/AticleService.js
+++ client/src/services/AticleService.js
@@ -2,12 +2,12 @@ import Api from '@/services/Api';
 
 export default {
   getArticles() {
-    return Api().get('article');
+    return Api().get('/article');
   },
   createArticle(article) {
-    return Api().post('article', article);
+    return Api().post('/article', article);
   },
   updateArticle(article, slug) {
-    return Api().patch(`article/edit/${slug}`, article);
+    return Api().patch(`/article/edit/${slug}`, article);
   },
 };
